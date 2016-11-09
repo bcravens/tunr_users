47ec8f18fd6b40d573db03fd52bdc96fa2a825ea
Bradley Cravens <bradleycravens@gmail.com>
2016-11-09 09:34:48 -0500

    inital commit

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,54 @@
+class ArtistsController < ApplicationController
+  # index
+  def index
+    @artists = Artist.all
+  end
+
+  # new
+  def new
+    redirect_to root_path unless @current_user
+    @artist = Artist.new
+  end
+
+  # create
+  def create
+    redirect_to root_path unless @current_user
+    @artist = Artist.create!(artist_params)
+    redirect_to @artist
+  end
+
+  #show
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  # edit
+  def edit
+    redirect_to root_path unless @current_user
+    @artist = Artist.find(params[:id])
+  end
+
+
+  # update
+  def update
+    redirect_to root_path unless @current_user
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+
+    redirect_to @artist
+  end
+
+  # destroy
+  def destroy
+    redirect_to root_path unless @current_user
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+
+    redirect_to artists_path
+  end
+
+  private
+  def artist_params
+    params.require(:artist).permit(:name, :photo_url, :nationality)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root to: 'artists#index'
+
+  resources :songs, except: [:new, :create]
+  resources :artists do
+    resources :songs, only: [:new, :create]
+  end
+  resources :users
+
+  resource :session
+end
